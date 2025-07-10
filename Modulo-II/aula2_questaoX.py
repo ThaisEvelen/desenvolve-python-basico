9200d0feb8a43f3c0a6440997da8488cb754788c
ThaisEvelen <thaisevelen@outlook.com>
2025-07-10 00:10:06 -0300

    Aula 2

---
+++ Modulo-II/aula2_questaoX.py
@@ -0,0 +1,41 @@
+#Questão 1
+x= 5
+y=2
+print(type(x))
+print(type(y))
+z=(x/y)
+print(z)
+print(type(z))
+
+#Questão 2
+resultado="O resultado é"
+Valor1=10
+Valor2= 3.5
+soma=(Valor1+Valor2)
+print(resultado)
+print(type(resultado))
+print(Valor1)
+print(type(Valor1))
+print(Valor2)
+print(type(Valor2))
+print(soma)
+print(type(soma))
+
+#Questão 3
+v1 = 10
+v2 = 20
+aux = v1    
+v1 = v2     
+v2 = aux    
+print("v1 =", v1)  
+print("v2 =", v2) 
+
+#Questão 4
+Juros= 1.01
+saldo= 500.
+saldo=saldo*Juros
+print(saldo)
+saldo=saldo*Juros
+print(saldo)
+saldo=saldo*Juros
+print(saldo)
