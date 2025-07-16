9e7c6b83dc3366362c9afc7e279f13cb5c394d3d
ThaisEvelen <thaisevelen@outlook.com>
2025-07-15 22:25:17 -0300

    exercicio modulo III

--- Modulo-III/aula2_questaoX.py
+++ Modulo-III/aula2_questaoX.py
@@ -18,8 +18,18 @@ print(16 <= idade <= 18 and jogou == "True" and venceu >= 1)
 personagem = input("Escolha a classe (guerreiro, mago ou arqueiro): ")
 força = int(input("Digite os pontos de força (de 1 a 20): "))
 magia = int(input("Digite os pontos de magia (de 1 a 20): "))
-guerreiro = ((força>=15)and(magia<=10))
-mago = ((força<=10)and (magia>=15))
-arqueiro =((5 <=força <=15)and(5<= magia <=15))
-personagem == guerreiro or mago or arqueiro
-print(guerreiro or mago or arqueiro)
+guerreiro = (força>=15)and(magia<=10) 
+mago = (força<=10)and (magia>=15)
+arqueiro =(5 <=força <=15)and(5<= magia <=15)
+print((personagem == "guerreiro" and guerreiro) or
+    (personagem == "mago" and mago) or
+    (personagem == "arqueiro" and arqueiro))
+
+#Questão 5
+genero= input("Digite seu genero F ou M :")
+idade1 = int(input("Digite sua idade: "))
+tempo = int(input("Digite seu tempo de serviço (em anos) :"))
+F= idade1>60 or tempo >30 or (idade1 ==60 and tempo >= 25)
+M= idade1>=65 or tempo >30 or (idade1 ==60 and tempo >= 25)
+print((genero =="F" and F) or
+      (genero == "M" and M))
