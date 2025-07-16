2c1026acc19e07a8c5c0703ef155a6cf4771efeb
ThaisEvelen <thaisevelen@outlook.com>
2025-07-15 23:15:57 -0300

    exercicio modulo 3

--- Modulo-III/teste.py
+++
@@ -1,7 +0,0 @@
-genero= input("Digite seu genero F ou M :")
-idade1 = int(input("Digite sua idade: "))
-tempo = int(input("Digite seu tempo de serviço (em anos) :"))
-F= idade1>60 or tempo >30 or (idade1 ==60 and tempo >= 25)
-M= idade1>65 or tempo >30 or (idade1 ==60 and tempo >= 25)
-print((genero =="F" and F) or
-      (genero == "M" and M))
