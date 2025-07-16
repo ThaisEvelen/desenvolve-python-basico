e0bb31d66599084fd4b0abd109c6d404dadea102
ThaisEvelen <thaisevelen@outlook.com>
2025-07-15 22:38:18 -0300

    correção

--- Modulo-III/aula2_questaoX.py
+++ Modulo-III/aula2_questaoX.py
@@ -29,6 +29,6 @@ print((personagem == "guerreiro" and guerreiro) or
 genero= input("Digite seu genero F ou M :")
 idade1 = int(input("Digite sua idade: "))
 tempo = int(input("Digite seu tempo de serviço (em anos) :"))
-F= idade1>=60 or tempo >30 or (idade1 ==60 and tempo >= 25)
-M= idade1>=65 or tempo >30 or (idade1 ==60 and tempo >= 25)
+F= idade1>=60 or tempo >=30 or (idade1 ==60 and tempo >= 25)
+M= idade1>=65 or tempo >=30 or (idade1 ==60 and tempo >= 25)
 print((genero =="F" and F) or (genero == "M" and M))
