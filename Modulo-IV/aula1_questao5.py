88921659c5702c9a5edf52b110c2627a48ffb7ab
ThaisEvelen <thaisevelen@outlook.com>
2025-07-16 22:37:46 -0300

    exercicio modulo 4

--- Modulo-IV/aula1_questao5.py
+++ Modulo-IV/aula1_questao5.py
@@ -1,7 +1,11 @@
-n= int(input("Digite a quantidade de respondentes: "))
-while n >1:
-    idade=int(input("Digite a idade do respondente"))
-    idade=idade+0
-    media =(idade)/n 
-    n = n-1
-print(media)
+n = int(input("Digite a quantidade de respondentes: "))
+soma = 0
+contador = n 
+
+while n > 0:
+    idade = int(input("Digite a idade do respondente: "))
+    soma += idade
+    n -= 1
+
+media = soma / contador
+print("A média das idades é:", media)
