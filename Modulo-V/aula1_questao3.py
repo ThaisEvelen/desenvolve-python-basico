0d860f2dc1e18d344e3a1f8843c3217d656de8a6
ThaisEvelen <thaisevelen@outlook.com>
2025-07-22 23:57:36 -0300

    modulo-5

--- Modulo-V/aula1_questao3.py
+++ Modulo-V/aula1_questao3.py
@@ -1,10 +1,10 @@
 import random
-valor=random.randint(1,11)
+valor=random.randint(1,10)
 n=int(input("Digite um valor entre 1 e 10: "))
 while valor !=n:
     if valor>n:
-        print("O número é menor que o valor digitado:")
+        print("O número é maior que o valor digitado:")
     elif valor<n:
-        print("O número é maior que o vaor digitado:")
-n=int(input("Digite um valor entre 1 e 10: "))
+        print("O número é menor que o valor digitado:")
+    n=int(input("Digite novamente um valor:"))
 print("Você acertou")
